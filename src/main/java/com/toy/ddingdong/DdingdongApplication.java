43dca72f25e9de685bb857e9e887ef839eb14157
kid7258 <kid7258@gmail.com>
2023-02-04 14:30:03 +0900

    프로젝트 생성

---
+++ src/main/java/com/toy/ddingdong/DdingdongApplication.java
@@ -0,0 +1,13 @@
+package com.toy.ddingdong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdingdongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdingdongApplication.class, args);
+	}
+
+}
